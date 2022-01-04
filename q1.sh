af10979a726f5cc35a5a661f034e65fbf393830b
lioreldar <liore888@gmail.com>
2022-01-04 11:33:49 +0200

    Lior ex10 solution

--- q1.sh
+++ q1.sh
@@ -1,1 +1,9 @@
-This is q1 answer
+(a) awk 'END{print NR}' aliceinwonderland.txt
+(b) awk '/Alice/ {print}' aliceinwonderland.txt | awk 'END{print NR}'
+(c) awk '/ Alice / {print}' aliceinwonderland.txt | awk 'END{print NR}'
+(d) awk '{a[$0]++} END{for (k in a) print k,a[k]}' RS='[[:space:]]+' aliceinwonderland.txt | awk ' /Alice/  {print}' | awk '{SUM+=$2}END {print SUM}'
+(e) awk '{print length $0}' aliceinwonderland.txt | sort -n | head -n 1
+(f) cat aliceinwonderland.txt | awk '{print tolower($0)}' | grep -oE '\w{1,}' | sort | uniq -c | sort -nr | head -n 5
+(g) awk '/Alice/' aliceinwonderland.txt | awk '/[0-9]/ {print}' | awk 'END{print NR}'
+ 
+
