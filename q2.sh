15ca130d125f4bcac63039cf374e89fbeb9529d4
roy madpis <roy.madpis@idc.ac.il>
2022-01-05 10:40:49 +0200

    change a lit bit the answers and deleted unnecceary things

--- q2.sh
+++ q2.sh
@@ -1,58 +1,32 @@
 #!/bin/bash
-#if you want to download the data:
-#wget https://raw.githubusercontent.com/rickygv99/gutenberg/master/data/original/sherlockholmes.txt
+#1a.print the number of lines in the book
+command1=$(awk 'END{print NR}' aliceinwonderland.txt) #= cat aliceinwonderland.txt | wc -l
+echo "Number of rows in the book = ${command1}"
 
-#2.a print the number of lines that has the word Sherlock or Holmes
-command1=$(sed -n -E '/(Sherlock|Holmes)/p' sherlockholmes.txt | wc -l)
-echo "Number of lines that has the word Sherlock / Holmes = ${command1}" #grep "Sherlock\|Holmes" sherlockholmes.txt  | wc -l
-#another option with sed - without using regexp - sed -n '/Holmes\|Sherlock/p' sherlockholmes.txt | wc -l
+#1.b print the number of rows that contain the word "Alice"
+command2=$(awk '/Alice/' aliceinwonderland.txt | awk 'END{print NR}') #grep -w "Alice" aliceinwonderland.txt  | wc -l
+echo "Number of rows that contain the word Alice = ${command2}"
 
-#2.b print the number of occurences of the words Sherlock + the number of occurences of the word Holmes
-command2=$( sed 's/[[:space:]]/\n/g' sherlockholmes.txt | sed -n -E '/(Sherlock|Holmes)/p' | wc -l)
-#grep -o "Sherlock\|Holmes" sherlockholmes.txt |wc -l
-echo "Number of occurences of the words Sherlock + Holmes = ${command2}"
+#1.c  print the number of time the word Alice appears in the book (appears with space before and after the word Alice")
+command3=$(awk '/ Alice /' aliceinwonderland.txt | awk 'END{print NR}') #grep -o " Alice " aliceinwonderland.txt | wc -l
+echo "Number of occurences of the word ' Alice ' with space before and after = ${command3}"
 
-#2.c write command that substitute the name Sherlock with other name - your choice
-#sed 's/Sherlock/Noa Kirel/g' sherlockholmes.txt
-#option for silence mode: only print the command of question 2.c
-if [[ ( "$#" -gt 0 &&  $1 = "s"  ) ]]; then
-	echo "command 3: sed 's/Sherlock/Noa Kirel/g' sherlockholmes.txt"
-elif [ "$#" -eq 0 ]; then
-	sed 's/Sherlock/Noa Kirel/g' sherlockholmes.txt
-fi
+#1.d print the number of times the word Alice appears, including the times it attached to other words
+#print each word in a different line:
+#cat aliceinwonderland.txt | tr ' ' '\n'
 
-#2.d print the 5 most common words that appear after the word Sherlock
-cat sherlockholmes.txt | sed -n 's/.*Sherlock //p' | cut -d " " -f 1 | tr -d "[[:punct:]]" | tr -d "’" | sort | uniq -c
-#### better with grep!!:
-echo "-----------------------"
-grep -oP "Sherlock\s+\K\w+" sherlockholmes.txt |sort | uniq -c | sort -n | tail -n 5
+command4=$(cat aliceinwonderland.txt | tr ' ' '\n'  | awk '/Alice/' | awk 'END{print NR}') #grep -o "Alice" aliceinwonderland.txt | wc -l 
+echo "Number of times the word Alice appear including comming near other words = ${command4}"
 
-#2.e write command that deletes the rows with full name of a person or place
-#*full name = two words that starts with a capital letter and the length of each of those words is 2 or more.
+#1.e print the smallest row in the book
+echo "Shortest row in the book:"
+awk '{print NF "-> is the number of characters in the row | The content of the row:", $0}' aliceinwonderland.txt | sort -n | head -n 1
+# another option: awk 'NR==1{x=$0}length($0)<length(x){x=$0}END{print x}' aliceinwonderland.txt 
 
-#make a backup:
-cat sherlockholmes.txt >  ssherlockholmes.txt
-#########################
-sed -i -E "/[A-Z][A-Za-z]+ [A-Z][A-Za-z]+/d" ./sherlockholmes.txt 
+#1.f print the top 5 most common words
+echo "Top 5 common words:"
+for word in $(cat aliceinwonderland.txt) ; do echo $word; done | awk '! /([[:punct:]]|[[:space:]])/' | awk '{a[$1]++}END {for (k in a) print a[k], k}' | sort -n -r | head -n 5
 
-
-#2.f write command that replace all the sentences in () to sentences in [] ##assume that such sentences start and end in the same line
-sed -i -E "s/\((.+)\)/\[\1\] /g" sherlockholmes.txt
-
-#to see the lines we substitue:
-#sed -nE "s/\((.+)\)/\[\1\] /gp" sherlockholmes.txt
-
-
-#grep -E "\(.+\)" sherlockholmes.txt
-#grep -E "\(.+\)" sherlockholmes.txt | sed -E "s/\((.+)\)/\[\1\] /g"
-
-
-#grep -E "[A-Z][A-Za-z]+ [A-Z][A-Za-z]+" sherlockholmes.txt | wc -l
-
-#sed -n -E "/[A-Z][A-Za-z]+ [A-Z][A-Za-z]+/p" sherlockholmes.txt | wc -l
-
-
-#cat sherlockholmes.txt | sed -n 's/.*Sherlock //' | cut -d " " -f 1
-#cat sherlockholmes.txt | sed -n 's/.*Sherlock //p' | cut -d " " -f 1
-#echo "--------------------"
-#cat sherlockholmes.txt | sed -n 's/.*Sherlock //p' | cut -d " " -f 1 | tr -d "[[:punct:]]" | tr -d "’" | sort | uniq -c
+#1.g print the number of rows containing a number and the word Alice
+awk '/[[:digit:]]/' aliceinwonderland.txt | awk '/Ailce/' | wc -l
+#grep -E "[[:digit:]]" aliceinwonderland.txt | grep Alice
