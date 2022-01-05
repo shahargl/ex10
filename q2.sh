06770005e5609aaf5c7447ea587ab6a850542e82
liamtadmor <96583989+liamtadmor@users.noreply.github.com>
2022-01-05 21:31:57 +0200

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -5,6 +5,6 @@ tr ' ' '\n' < sherlockholmes.txt | sed -n -E '/(Sherlock|Holmes)/p' | wc -l
 # answer c:
 sed 's/Sherlock/Liam/g' sherlockholmes.txt
 # answer e:
-sed -i -E '/[A-Z][a-z]+ [A-Z][a-z]+/d' sherlockholmes.txt
+sed -E '/[A-Z][a-z]+ [A-Z][a-z]+/d' sherlockholmes.txt
 # answer f
 sed 's/(\([a-zA-Z0-9]*\))/[\1]/g' sherlockholmes.txt
